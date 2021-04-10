5b0cfb90a71afa15cbca2772b09b14ed1352f755
Paul Schlueter <paul@paulschlueter.com>
2021-04-10 16:30:21 -0500

    Setup RDS Database (#3)
    * WIP RDS Setup
    
    * Set DB Username & Password
    
    * Add SSM Parameters

---
+++ terraform/rds/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws",
+      version = "3.4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "paul-terraform-backends"
+    key    = "signup-api-rds.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "DB_USERNAME" {
+  # Environment variable set in the GitHub actions pipeline
+  type = string
+}
+
+variable "DB_PASSWORD" {
+  # Environment variable set in the GitHub actions pipeline
+  type = string
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name       = "rds-subnet-group"
+  subnet_ids = ["subnet-03a512caca489d92e"]
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name        = "db_password"
+  description = "Database password"
+  type        = "SecureString"
+  value       = var.DB_PASSWORD
+}
+
+resource "aws_ssm_parameter" "db_username" {
+  name        = "db_username"
+  description = "Database username"
+  type        = "SecureString"
+  value       = var.DB_USERNAME
+}
+
+resource "aws_db_instance" "prod" {
+  allocated_storage       = 20 # gigabytes
+  backup_retention_period = 5  # in days
+  db_subnet_group_name    = aws_db_subnet_group.rds.name
+  engine                  = "postgres"
+  engine_version          = "12.5"
+  identifier              = "prod"
+  instance_class          = "db.t2.micro"
+  multi_az                = false
+  name                    = "prod-main"
+  password                = var.DB_PASSWORD
+  port                    = 5432
+  publicly_accessible     = true
+  storage_encrypted       = true # you should always do this
+  storage_type            = "gp2"
+  username                = var.DB_USERNAME
+  vpc_security_group_ids  = ["sg-0255e56b16aeefad8"]
+}
