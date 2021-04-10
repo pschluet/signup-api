e22ce82de235fb0de80d062b5b2f618315dedbea
Paul Schlueter <paul@paulschlueter.com>
2021-04-10 08:09:40 -0500

    Add Network Terraform (#2)

---
+++ terraform/network/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws",
+      version = "3.4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "paul-terraform-backends"
+    key    = "signup-api-network.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "route-us-east-1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "us-east-1" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.route-us-east-1.id
+}
+
+resource "aws_security_group" "trusted" {
+  name        = "trusted-security-group"
+  vpc_id      = aws_vpc.main.id
+  description = "Trusted security group for trusted addresses"
+
+  ingress {
+    description = "Allow internal access"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds-postgres" {
+  name = "rds-postgres"
+
+  description = "RDS postgres servers (terraform-managed)"
+  vpc_id      = aws_vpc.main.id
+
+  # Only postgres in
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
