0f79f528e3a43dff8cdd084bfaf17b118dfce57c
Paul Schlueter <paul@paulschlueter.com>
2021-04-10 16:48:31 -0500

    Add New Subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -27,7 +27,12 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
 }
 
 resource "aws_internet_gateway" "main" {
